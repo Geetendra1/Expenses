0c35831bd6b015d78a26ace59168e93e4ec84e08
Geetendra1 <sengargeetendra123@gmail.com>
2020-08-14 13:43:28 +0000

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <h1>
+      Expense Tracer
+    </h1>
+  );
+}
+
+
---
+++ src/context/GlobalState.js
@@ -0,0 +1,42 @@
+import React,{createContext,useReducer} from 'react';
+import AppReducer from './AppReducer'
+// income is positive digit & expence is negative -ve digit
+const initialState = {
+    transactions:  [
+  { id: 1, text: 'Flower', amount: -20 },
+  { id: 2, text: 'Salary', amount: 300 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 }
+    ]
+}
+
+// Create context 
+export const GlobalContext = createContext(initialState);
+
+// Provider 
+export const GlobalProvider = ({children}) => {
+   const [state,dispatch] = useReducer(AppReducer, initialState);
+
+// Actions list 
+function deleteTransaction(id) {
+    dispatch({
+        type:'DELETE_TRANSACTION',
+        payload: id
+    });
+}
+
+function addTransaction(transaction) {
+    dispatch({
+        type:'ADD_TRANSACTION',
+        payload:transaction
+    })
+}
+
+   return (<GlobalContext.Provider value={{
+       transactions: state.transactions,
+       deleteTransaction,
+       addTransaction
+   }}>
+       {children}
+   </GlobalContext.Provider>)
+}
